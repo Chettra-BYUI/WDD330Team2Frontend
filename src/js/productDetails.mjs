3a21783ae7b1c9b9100d2dcc604ab96d165b6203
Zamza584 <mzamalloa97@gmail.com>
2023-05-15 22:45:40 -0500

    Completed product not found page

--- src/js/productDetails.mjs
+++ src/js/productDetails.mjs
@@ -4,12 +4,31 @@ import { findProductById } from "./productData.mjs";
 let productData;
 
 export default async function productDetails(productId, selector) {
-  productData = await findProductById(productId);
-  renderProductDetails(productData);
-  document
-    .getElementById(selector)
-    .addEventListener("click", addToCartHandler);
+  productData = await findProductById(productId, "tents");
+  if (productData) {
+    renderProductDetails(productData);
+    document
+      .getElementById(selector)
+      .addEventListener("click", addToCartHandler);
+  } else {
+    //Product not found page
+    //Used only when product id is not found. 
 
+    document.getElementById("addToCart").remove();
+    const productDetail = document.querySelector(".product-detail");
+    productDetail.innerHTML = "";
+
+    const status = document.createElement("p");
+    const title = document.createElement("h1");
+    
+    status.style.cssText =
+      "text-align: center; color: #DD5858; font-size: 100px; margin: 20px";
+    title.style.cssText = "text-align: center; color: #DD5858";
+
+    status.innerHTML = "404";
+    title.innerHTML = "Uh-Oh... Sorry your product is not found";
+    productDetail.innerHTML += status.outerHTML + title.outerHTML;
+  }
 }
 
 // add to cart button event handler
@@ -23,15 +42,24 @@ function addProductToCart(productDetail) {
 
 function renderProductDetails(productDetail) {
   document.getElementById("productName").innerHTML = productDetail.Brand.Name;
-  document.getElementById("productNameWithoutBrand").innerHTML = productDetail.Name;
+  document.getElementById("productNameWithoutBrand").innerHTML =
+    productDetail.Name;
 
-  document.getElementById("productImage").setAttribute("src", productDetail.Image);
-  document.getElementById("productImage").setAttribute("alt", productDetail.Name);
+  document
+    .getElementById("productImage")
+    .setAttribute("src", productDetail.Image);
+  document
+    .getElementById("productImage")
+    .setAttribute("alt", productDetail.Name);
 
-  document.getElementById("productColorName").innerHTML = productDetail.Colors[0].ColorName;
-  document.getElementById("productDescriptionHtmlSimple").innerHTML = productDetail.DescriptionHtmlSimple;
+  document.getElementById("productColorName").innerHTML =
+    productDetail.Colors[0].ColorName;
+  document.getElementById("productDescriptionHtmlSimple").innerHTML =
+    productDetail.DescriptionHtmlSimple;
 
-  document.getElementById("addToCart").setAttribute("data-id", productDetail.Id);
+  document
+    .getElementById("addToCart")
+    .setAttribute("data-id", productDetail.Id);
 
   // const productElement = document.querySelector(".product-detail");
   // const productNode = `
@@ -47,5 +75,4 @@ function renderProductDetails(productDetail) {
   // `;
 
   // productElement.insertAdjacentHTML("afterbegin", productNode);
-
 }
